a6aa79fe593d70715355fa0b326e2f2d6ce3d802
H <hieu30029081@gmail.com>
2024-12-05 19:03:32 +0700

    add image

--- setup.sh
+++ setup.sh
@@ -14,3 +14,6 @@ mv memory /workspace/MeaCap/data
 mv checkpoints /workspace/MeaCap/data
 pip install transformers
 pip install sentence-transformers
+
+#Run scripts
+python inference.py --use_prompt  --memory_id cc3m --img_path ./image_example --lm_model_path ./data/checkpoints/CBART_one_billion --vl_model openai/clip-vit-base-patch32 --wte_model_path sentence-transformers/all-MiniLM-L6-v2 --parser_checkpoint lizhuang144/flan-t5-base-VG-factual-sg
